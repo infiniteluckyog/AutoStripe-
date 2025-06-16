5df73bda4a70978fdb44e3247ce00a8b954e1d89
infiniteluckyog <for.no.who@gmail.com>
2025-06-16 13:00:50 +0530

    Update autostripe.py

--- autostripe.py
+++ autostripe.py
@@ -1,15 +1,7 @@
-from quart import Quart, request, jsonify
-import httpx
-import asyncio
+from quart import Quart
 
 app = Quart(__name__)
 
-@app.route('/test')
-async def test():
+@app.route('/')
+async def home():
     return 'OK'
-
-@app.route('/stripe_raw', methods=['GET'])
-async def stripe_raw():
-    return jsonify({'success': True})
-
-# Do NOT put `app = "anything"` anywhere else in the file!
